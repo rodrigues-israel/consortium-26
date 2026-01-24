114523ebce287fdd2641a6523333fbfa87bc0923
devisrael26 <israelmmm2006@gmail.com>
2026-01-25 00:58:28 +0530

    fixed eventtext

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -21,7 +21,7 @@ export default function Event(props: EventProps) {
         <div className="eventTitle text-white font-montserrat font-bold text-[2.5rem] p-3 translate-y-10 transition-transform group-hover:translate-y-0 group-hover:ease-out md:text-[2rem] lg:text-[1.4rem] text-center">
           {props.eventTitle}
         </div>
-        <div className="eventText h-35 max-w-90 text-center text-white font-montserrat p-5 pt-0 opacity-0 transition-transform group-hover:opacity-100 group-hover:ease-out duration-100 delay-50">
+        <div className="eventText h-35 max-w-90 text-center text-white font-montserrat pt-0 opacity-0 transition-transform group-hover:opacity-100 group-hover:ease-out duration-100 delay-50">
           {props.eventText}
         </div>
         <a
